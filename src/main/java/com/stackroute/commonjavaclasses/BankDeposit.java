c29571e760138c87ecf10d94589999cfeb97e9c4
mitaligarg10 <mitaligarg94@gmail.com>
2020-10-14 17:10:50 +0530

    updater project structure, pom.xml, problem.md file

---
+++ src/main/java/com/stackroute/commonjavaclasses/BankDeposit.java
@@ -0,0 +1,5 @@
+package com.stackroute.commonjavaclasses;
+
+/* Complete the class as per the requirements given in PROBLEM.md */
+public class BankDeposit {
+}
---
+++ src/test/java/com/stackroute/commonjavaclasses/BankDepositTests.java
@@ -0,0 +1,5 @@
+package com.stackroute.commonjavaclasses;
+
+/* Write test cases for positive and negative scenarios*/
+public class BankDepositTests {
+}
